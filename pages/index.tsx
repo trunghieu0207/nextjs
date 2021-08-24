1b8ec13afbae676bd79d6e477e6f36976da53cdf
Hieu Dang <dangtrunghieu0207@gmail.com>
2021-08-24 08:46:03 +0700

    Delete p tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,12 +12,6 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
