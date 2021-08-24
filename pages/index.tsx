6841c01e95759cf78aca35c4cf60120473486c24
Hieu Dang <dangtrunghieu0207@gmail.com>
2021-08-24 08:46:30 +0700

    Delete a tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,11 +18,6 @@ export default function Home() {
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
