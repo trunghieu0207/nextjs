c6f0ff19d47d0edb12092af2fd59566f0211dae8
Hieu Dang <dangtrunghieu0207@gmail.com>
2021-08-24 08:45:34 +0700

    Delete h1

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,6 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
